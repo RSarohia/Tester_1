d7a2f26b382dfbeda4b271b9a8c1349a3f7ec947
Richard Sarohia <RSarohia@hotmail.com>
2020-02-18 21:50:21 -0500

    Third Update

--- app.js
+++ app.js
@@ -1,5 +1,17 @@
 const express = require('express');
 const bodyparser = require('body-parser');
 const request = require('request');
+const ejs = require('ejs');
 
-const app = express;
+const app = express();
+app.use(bodyparser.urlencoded({extended:false}));
+
+app.set('view engine', 'ejs');
+
+let user1 = "Richard";
+
+app.get('/',function(req,res) {
+    res.render('index',{user:user1});
+})
+
+app.listen(3000, console.log("Server is listening on Port 3000"));
